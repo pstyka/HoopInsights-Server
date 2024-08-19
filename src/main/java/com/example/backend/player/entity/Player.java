b47728c0d7d2fa2619d5a230a6f06ead9ed79b7f
pawel <pawel@styka.pl>
2024-08-19 21:15:23 +0200

    player entity edit

--- src/main/java/com/example/backend/player/entity/Player.java
+++ src/main/java/com/example/backend/player/entity/Player.java
@@ -22,22 +22,43 @@ public class Player {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")  // Opcjonalnie można określić nazwę kolumny, choć domyślnie jest to 'id'
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
     private String lastName;
+
+    @Column(name = "position", nullable = false, length = 20)
     private String position;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "birth_date", nullable = false)
     private Date birthDate;
+
+    @Column(name = "height_inch", nullable = false, length = 10)
     private String heightInch;
+
+    @Column(name = "weight_lbs", nullable = false, length = 10)
     private String weightLbs;
+
+    @Column(name = "country", nullable = false, length = 50)
     private String country;
+
+    @Column(name = "jersey_number", nullable = false)
     private int jerseyNumber;
+
+    @Column(name = "is_active", nullable = false)
     private Boolean isActive;
 
-    @ManyToOne
-    @JoinColumn(name = "team_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id", nullable = false)
     private Team team;
 
     @OneToMany(mappedBy = "player", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("season ASC")
     private List<PlayerSeasonStats> playerSeasonStats = new ArrayList<>();
 
     @OneToOne(mappedBy = "player", cascade = CascadeType.ALL, orphanRemoval = true)
